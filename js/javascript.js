db7ddb7ae2178958d3ab778b78a7683d1073a751
ujunghee <86220815+ujunghee@users.noreply.github.com>
2023-08-27 21:47:55 +0900

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -62,7 +62,7 @@ document.addEventListener("DOMContentLoaded", function () {
 // 스크롤 스무스 이벤트
 const lenis = new Lenis({
   duration: 1.2,
-  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -5 * t)), // https://www.desmos.com/calculator/brs54l4xou
   direction: 'vertical', // vertical, horizontal
   gestureDirection: 'vertical', // vertical, horizontal, both
   smooth: true,
